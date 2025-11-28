1f8b57affb7d580235527af801d10d30f09d2c3c
Ali-Alaalem <meemo2002a@gmail.com>
2025-11-28 20:41:28 +0300

    adding setters and getters in User Class

--- src/User.java
+++ src/User.java
@@ -6,4 +6,44 @@ public abstract class User implements IRegistrtion {
     public String email;
     public String hashed_pass;
 
+
+    public String getF_name() {
+        return f_name;
+    }
+
+    public void setF_name(String f_name) {
+        this.f_name = f_name;
+    }
+
+    public String getL_name() {
+        return l_name;
+    }
+
+    public void setL_name(String l_name) {
+        this.l_name = l_name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHashed_pass() {
+        return hashed_pass;
+    }
+
+    public void setHashed_pass(String hashed_pass) {
+        this.hashed_pass = hashed_pass;
+    }
 }
